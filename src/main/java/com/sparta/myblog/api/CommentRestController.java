683da92983541e76b82e82f744fbccb401762b5a
wowba <87873821+wowba@users.noreply.github.com>
2022-02-16 21:31:35 +0900

    error update

--- src/main/java/com/sparta/myblog/api/CommentRestController.java
+++ src/main/java/com/sparta/myblog/api/CommentRestController.java
@@ -1,7 +1,6 @@
 package com.sparta.myblog.api;
 
 import com.sparta.myblog.dto.CommentRequestDto;
-import com.sparta.myblog.dto.CommentResponseDto;
 import com.sparta.myblog.model.Comment;
 import com.sparta.myblog.model.Posting;
 import com.sparta.myblog.repository.CommentRepository;
@@ -19,9 +18,9 @@ public class CommentRestController {
     private final CommentRepository commentRepository;
     private final CommentService commentService;
 
-//     댓글 생성하기
+    // 댓글 생성하기 ( 에러메세지 리턴하기 )
     @PostMapping("/api/postings/{id}/comments")
-    public CommentResponseDto createComment(@RequestBody CommentRequestDto commentRequestDto, @PathVariable Long id) throws Exception {
+    public @ResponseBody String createComment(@RequestBody CommentRequestDto commentRequestDto, @PathVariable Long id){
         // 댓글 다는 게시판 가져오기
         Posting posting = postingRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
@@ -30,18 +29,15 @@ public class CommentRestController {
         Comment comment = new Comment(commentRequestDto, posting);
         // 댓글 내용이 공백인지 확인
         if(comment.getComment() == ""){
-            throw new Exception ("댓글 내용을 입력해주세요");
+            return "댓글 내용을 입력해주세요.";
         }
         commentRepository.save(comment);
-
-        // responseDto 만들어서 리턴하기.
-        CommentResponseDto commentResponseDto = new CommentResponseDto(commentRequestDto);
-        return commentResponseDto;
+        return "댓글이 작성되었습니다.";
     }
 
     // 댓글 수정하기
     @PutMapping("/api/comments/{id}")
-    public Long editComment(@RequestBody CommentRequestDto commentRequestDto, @PathVariable Long id) throws Exception {
+    public @ResponseBody String editComment(@RequestBody CommentRequestDto commentRequestDto, @PathVariable Long id){
         return commentService.Edit(id, commentRequestDto);
     }
 
--- src/main/java/com/sparta/myblog/service/CommentService.java
+++ src/main/java/com/sparta/myblog/service/CommentService.java
@@ -5,6 +5,7 @@ import com.sparta.myblog.model.Comment;
 import com.sparta.myblog.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.transaction.Transactional;
 
@@ -15,15 +16,15 @@ public class CommentService {
     
     // 댓글 수정하기
     @Transactional
-    public Long Edit(Long id, CommentRequestDto commentRequestDto) throws Exception {
+    public @ResponseBody String Edit(Long id, CommentRequestDto commentRequestDto){
         Comment comment = commentRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
         // 댓글 내용이 공백인지 확인
         if(commentRequestDto.getComment() == ""){
-            throw new Exception ("댓글 내용을 입력해주세요");
+            return "댓글 내용을 입력해주세요.";
         }
         comment.Edit(commentRequestDto);
-        return id;
+        return "댓글 수정 완료.";
     }
 
     // 댓글 삭제하기
